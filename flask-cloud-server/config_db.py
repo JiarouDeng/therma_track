212d379b47c5f4a5d23680840636198e910ac0fb
bella-xia <zxia15@jh.edu>
2025-03-19 21:46:58 -0400

    loll sorry no hard coded data

--- flask-cloud-server/config_db.py
+++ flask-cloud-server/config_db.py
@@ -5,11 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 def config_db():
     app = Flask(__name__)
 
-    SERVER_HOST = "free-db2.cnq40qooeq8t.us-east-2.rds.amazonaws.com"
-    PORT_NUM = "3306"
-    USERNAME = "root"
-    PASSWORD = "root200063^"
-    DB_NAME = "therma_track"
+    SERVER_HOST = ""
+    PORT_NUM = ""
+    USERNAME = ""
+    PASSWORD = ""
+    DB_NAME = ""
     DB_URL = f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{SERVER_HOST}:{PORT_NUM}/{DB_NAME}"
     app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
